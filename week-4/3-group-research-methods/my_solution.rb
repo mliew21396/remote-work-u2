edde6c255eea61766d3fd906a1bc1b18a899b8f0
Maria Piper <maria@devbootcamp.com>
2014-09-03 13:27:11 -0700

    added note about numbers to strings

--- week-4/3-group-research-methods/my_solution.rb
+++ week-4/3-group-research-methods/my_solution.rb
@@ -71,19 +71,24 @@ end
 p my_array_finding_method(i_want_pets, "t") == ["want","pets","but"]
 p my_hash_finding_method(my_family_pets_ages, 3) == ["Hoobie", "Ditto"]
 
-#Person 2
+# Person 2
 p my_array_modification_method(i_want_pets, 1) == ["I", "want", 4, "pets", "but", "only", "have", 3 ]
 p my_hash_modification_method(my_family_pets_ages, 2) == {"Evi" => 8, "Hoobie" => 5, "George" => 14, "Bogart" => 6, "Poly" => 6, "Annabelle" => 2, "Ditto" => 5}
 
-#Person 3
-#This driver code will only pass if you have the code from Person 2! If you don't have it, copy/modify it so you can
-#test whether your method works.
+# Person 3
+# This driver code will only pass if you have the code from Person 2! If you don't have it, copy/modify it so you can
+# test whether your method works.
+
+# The numbers were converted to strings because of the method I used when I made this. 
+# You don't have to do this in the exact same way, just make sure to write your own 
+# test if you are going to do it differently and give that to your group.
+
 p my_array_sorting_method(i_want_pets) == ["3", "4", "I", "but", "have", "only", "pets", "want"]
 p my_hash_sorting_method(my_family_pets_ages) == [["Annabelle", 2], ["Ditto", 5], ["Hoobie", 5], ["Bogart", 6], ["Poly", 6], ["Evi", 8], ["George", 14]] 
 
-#Person 4
-#This driver code will only pass if you have the code from Person 2!If you don't have it, copy/modify it so you can
-#test whether your method works.
+# Person 4
+# This driver code will only pass if you have the code from Person 2!If you don't have it, copy/modify it so you can
+# test whether your method works.
 # This may be false depending on how your method deals with ordering the animals with the same ages.
 p my_array_deletion_method(i_want_pets, "a") == ["I", 4, "pets", "but", "only", 3 ]
 p my_hash_deletion_method(my_family_pets_ages, "George") == {"Evi" => 8, "Hoobie" => 5, "Bogart" => 6, "Poly" => 6, "Annabelle" => 2, "Ditto" => 5}

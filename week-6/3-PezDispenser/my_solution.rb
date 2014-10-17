af0d025cb810b3c85d36b9e4e3d4e313786ec718
Maria Piper <mbtomori@gmail.com>
2014-10-17 15:50:54 -0700

    renumbered solution file to match releases

--- week-6/3-PezDispenser/my_solution.rb
+++ week-6/3-PezDispenser/my_solution.rb
@@ -2,13 +2,13 @@
 
 
 # 1. Review the following user stories:
-# - As a pez user, I'd like to be able to "create" a new pez dispenser with a group of flavors that 
+# - As a pez user, I'd like to be able to "create" a new pez dispenser with a group of flavors that
 #      represent pez so it's easy to start with a full pez dispenser.
-# - As a pez user, I'd like to be able to easily count the number of pez remaining in a dispenser 
+# - As a pez user, I'd like to be able to easily count the number of pez remaining in a dispenser
 #      so I can know how many are left.
 # - As a pez user, I'd like to be able to take a pez from the dispenser so I can eat it.
 # - As a pez user, I'd like to be able to add a pez to the dispenser so I can save a flavor for later.
-# - As a pez user, I'd like to be able to see all the flavors inside the dispenser so I know the order 
+# - As a pez user, I'd like to be able to see all the flavors inside the dispenser so I know the order
 #      of the flavors coming up.
 
 
@@ -19,14 +19,14 @@
 # 3. Initial Solution
 
 class PezDispenser
- 
+
 # your code here!
- 
+
 end
- 
 
 
-# 4. Refactored Solution
+
+# 5. Refactored Solution
 
 
 
@@ -37,9 +37,9 @@ end
 
 flavors = %w(cherry chocolate cola grape lemon orange peppermint raspberry strawberry).shuffle
 super_mario = PezDispenser.new(flavors)
-puts "A new pez dispenser has been created. You have #{super_mario.pez_count} pez!"  
-puts "Here's a look inside the dispenser:"  
-puts super_mario.see_all_pez 
+puts "A new pez dispenser has been created. You have #{super_mario.pez_count} pez!"
+puts "Here's a look inside the dispenser:"
+puts super_mario.see_all_pez
 puts "Adding a purple pez."
 super_mario.add_pez("purple")   # mmmmm, purple flavor
 puts "Now you have #{super_mario.pez_count} pez!"
@@ -50,4 +50,4 @@ puts "Now you have #{super_mario.pez_count} pez!"
 
 
 
-# 5. Reflection 
+# 6. Reflection

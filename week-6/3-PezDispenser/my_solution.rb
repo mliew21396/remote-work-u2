e87e9aa659933622c986ef4a2e71c028103c184f
Maria Piper <maria@devbootcamp.com>
2015-03-13 10:06:08 -0700

    change purple to banana

--- week-6/3-PezDispenser/my_solution.rb
+++ week-6/3-PezDispenser/my_solution.rb
@@ -43,8 +43,8 @@ super_mario = PezDispenser.new(flavors)
 puts "A new pez dispenser has been created. You have #{super_mario.pez_count} pez!"
 puts "Here's a look inside the dispenser:"
 puts super_mario.see_all_pez
-puts "Adding a purple pez."
-super_mario.add_pez("purple")   # mmmmm, purple flavor
+puts "Adding a banana pez."
+super_mario.add_pez("banana") 
 puts "Now you have #{super_mario.pez_count} pez!"
 puts "Oh, you want one do you?"
 puts "The pez flavor you got is: #{super_mario.get_pez}"

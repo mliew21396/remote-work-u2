826ff46ada424f745518b7ff3dbec70b0613ea4f
Mathew Liew <mliew21396@gmail.com>
2015-07-09 13:13:12 -0500

    Add gps solution

--- week-4/gps2-1/my_solution.rb
+++ week-4/gps2-1/my_solution.rb
@@ -1,21 +1,48 @@
+
 # U2.W4: Pretty Triangle GPS 2.1
 
-# I worked on this challenge with: [].
+# I worked on this challenge with: Tanya.
 
-# I spent [] hours on this challenge.
+# I spent 0.75 hours on this challenge.
 
 # Pseudocode
+# Input: integer argument
+# Output: a right triangle using asterisk(s) in rows, lower integer to higher integer
+# Steps
+# 1. Initialize a counter, to count through the integer rows
+# 2. puts to print asterisks out to screen
 
 
 # Initial Solution
-
+#Solution 1
 def print_triangle(rows)
-
-  # Your code goes here!
-
+  if rows <= 0
+    raise ArgumentError.new("Only non-negative numbers are allowed")
+  else
+    rows.times {|x| puts "*" * (x+1)}
+  end
 end
 
-# Refactored solution
+# rows.times do ||
+#   puts "*" * (rows-i)
+#     i -= 1
+# end
+
+#Solution 2
+# def print_triangle(rows)
+#   unless rows > 0
+#     raise ArgumentError.new("Only non-negative numbers are allowed")
+#   end
+#   counter = 1
+#   while counter <= rows
+#     puts "*" * counter
+#     counter += 1
+#   end
+# end
 
 
-# Reflection
+print_triangle(5)
+
+
+
+
